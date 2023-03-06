17236642784d45992a96199a36dd0b6af00324c8
rangapv <rangapv@yahoo.com>
2023-03-06 12:55:28 +0000

    Added some checks before installs and default version number

--- installtf.sh
+++ installtf.sh
@@ -6,7 +6,6 @@ instf() {
 if [ "$#" -eq 0 ]
 then
 	echo "No terraform version mentioned"
-	exit
 fi
 
 ver="$@"
@@ -22,4 +21,31 @@ echo "$(terraform -v)"
 
 }
 
-instf $1
+
+versionchk () { 
+tf1=`terraform version`
+tf1s="$?"
+tf2=`which terraform`
+tf2s="$?"
+
+if [[ ($tf1s -eq  0) && ($tf2s -eq 0) ]]
+then
+	echo "$tf1 \n $tf2\n"
+else
+        echo "No terraform installed/found"
+	echo "To install the version 1.3.7 press enter or ENTER the required vrsion number"
+	read tg
+	#read -s -N 1 -t 1 tg
+        if [ "$tg" ==  $'\0A' ]
+	then
+		set -- "1.3.7"
+        fi
+       instf $1
+fi
+
+}
+
+
+ echo "Run this script as ./isstalltf.sh 1.3.7"
+
+ versionchk
