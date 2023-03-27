d1ab3d3640fd9f04c7f845fafc44ecc5ba1d6e58
rangapv <rangapv@yahoo.com>
2023-03-27 13:12:34 +0000

    This is the file which creates Policy,Roles and Entity that can asuume this Role in AWS

---
+++ ec2-simplek8s/role.tf
@@ -0,0 +1,32 @@
+#define Roles and Policies to be associated with the ec2-instance
+
+resource "aws_iam_policy" "policy1" {
+  # ... other configuration ...
+  path = "/users/rangapv/"
+  policy = "${file("./policy1.json")}"
+
+}
+
+resource "aws_iam_policy" "policy2" {
+  # ... other configuration ...
+  path = "/users/rangapv/"
+  policy = "${file("./policy2.json")}"
+
+}
+resource "aws_iam_role" "k8s_role" {
+  name = "k8s_role"
+  path = "/users/rangapv/"
+#  assume_role_policy = aws_iam_policy.policy1, 
+  assume_role_policy = "${file("./policy6.json")}"
+  managed_policy_arns = [ aws_iam_policy.policy1.arn, aws_iam_policy.policy2.arn ]
+# Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  tags = {
+    tag-key = "k8s-tf-role"
+  }
+}
+
+resource "aws_iam_instance_profile" "k8s_profile" {
+  name = "k8s_profile"
+  role = aws_iam_role.k8s_role.name
+}
