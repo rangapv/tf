8bcc3e3173e3b4ba95b1501b70436fd1f9231c3d
rangapv <rangapv@yahoo.com>
2023-04-03 13:01:04 +0000

    Applies universally across RANGAPV's aws account/ just modify secret.tfvars appropriately

--- ec2-vanilla/ec2.tf
+++ ec2-vanilla/ec2.tf
@@ -12,31 +12,32 @@ terraform {
 
 provider "aws" {
   region = var.region 
-  access_key = "*****"
-  secret_key = "*****"
+  access_key = var.accesskey 
+  secret_key = var.secretkey 
 }
 
-
-resource "aws_instance" "app_server" {
+resource "aws_instance" "rayapp_server" {
   ami           = var.ami 
   instance_type = var.ins_type 
   associate_public_ip_address = "true"
   #cpu_core_count = var.cpu_core
   key_name = var.key_name
-  security_groups = [ var.sec_name, ]
+  #security_groups = [ var.sec_name, ]
   user_data = "${file("./user1.sh")}"
   #volume_size = var.vol_size
+  vpc_security_group_ids = [ var.vpcid, ]
+  subnet_id = var.subid
   root_block_device {
     volume_size = var.vol_size
   }
   tags = {
-    Name = "AppServerInstance2"
+    Name = "RayappServerInstance2"
   }
 }
 
 
 output "instances" {
-  value       = "${aws_instance.app_server.*.public_ip}"
+  value       = "${aws_instance.rayapp_server.*.public_ip}"
   description = "PrivateIP address details"
 }
 
